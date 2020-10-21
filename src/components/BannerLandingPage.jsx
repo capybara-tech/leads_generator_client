2c094694e59092d9efd0fad4df05919f1301455c
felixoppongwillborg <fwlixw@gmail.com>
2020-10-21 14:13:28 +0200

    adding dummy hardcoded text in middle of image and button to read more inside banner image

--- src/components/BannerLandingPage.jsx
+++ src/components/BannerLandingPage.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import {Button} from "semantic-ui-react"
 
 function BannerLandingPage() {
   return (
-    <div>
+    <div id="bannerDiv">
       <img
         id="bannerImg"
-        src="/images/pexels-rodolfo-clix-1036936.jpg"
+        src="/images/BannerImage.jpg"
         alt="Top banner"
       />
+      <h1 id="bannerTxt">We will donate 167.450 kr to help save the planet</h1>
+      <Button id="bannerButton">Read more</Button>
     </div>
   );
 }
