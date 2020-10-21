48553a865762f82c3e397c5bd8e004b1563f4ee2
felixoppongwillborg <fwlixw@gmail.com>
2020-10-21 16:23:21 +0200

    added z-index to navbar so it'll be on top of image text now follows image

--- src/components/BannerLandingPage.jsx
+++ src/components/BannerLandingPage.jsx
@@ -3,14 +3,16 @@ import {Button} from "semantic-ui-react"
 
 function BannerLandingPage() {
   return (
-    <div id="bannerDiv">
+    <div id="banner">
       <img
         id="bannerImg"
         src="/images/BannerImage.jpg"
         alt="Top banner"
       />
-      <h1 id="bannerTxt">We will donate 167.450 kr to help save the planet</h1>
-      <Button id="bannerButton">Read more</Button>
+      <div id="bannerDiv">
+      <h1>We will donate 167.450 kr to help save the planet</h1>
+      <Button>Read more</Button>
+      </div>
     </div>
   );
 }
