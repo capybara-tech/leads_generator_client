7aed3dc1c3a3921f973f7b29d49ae9c818f6da97
Alex_Bellamy <alexlbellamy@gmail.com>
2020-10-22 10:53:16 +0200

    adding react final form package, creating initial input fields on quotespage component, with one rendering after text had been put into the first one

--- src/components/QuotesPage.jsx
+++ src/components/QuotesPage.jsx
@@ -1,10 +1,62 @@
 import React from "react";
+import { Form, Field } from "react-final-form";
+
+const onSubmit = () => {};
 
 const QuotesPage = () => {
   return (
-    <div>
+    <>
       <h1 data-cy="title">Get up to three quotes</h1>
-    </div>
+      <Form
+        onSubmit={onSubmit}
+        initialValues={{
+          name: null,
+          email: null,
+        }}
+        render={(props) => {
+          const { handleSubmit, form, values } = props;
+          return (
+            <form data-cy="quote-form" onSubmit={handleSubmit}>
+              <label>My name's Felix, what's yours?</label>
+              <br />
+              <Field
+                data-cy="name"
+                name="name"
+                component="input"
+                type="text"
+                placeholder="Name"
+              ></Field>
+
+              {values.name && values.name !== "false" && (
+                <Field
+                  data-cy="email"
+                  name="email"
+                  component="input"
+                  type="text"
+                  placeholder="Email"
+                >
+                  {({ input }) => {
+                    return (
+                      <>
+                        {" "}
+                        <br />
+                        <label>
+                          Thanks {values.name}, and your email address?
+                        </label>{" "}
+                        <br />
+                        {values.name && values.name !== "false" && (
+                          <input {...input} />
+                        )}
+                      </>
+                    );
+                  }}
+                </Field>
+              )}
+            </form>
+          );
+        }}
+      />
+    </>
   );
 };
 
