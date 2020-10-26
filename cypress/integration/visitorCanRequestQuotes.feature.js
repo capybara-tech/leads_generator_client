9d42110386a074409ffee6cfdbf5fec347a215f3
Alex_Bellamy <alexlbellamy@gmail.com>
2020-10-26 07:30:39 +0100

    adding sad path tests and amending current tests

--- cypress/integration/visitorCanRequestQuotes.feature.js
+++ cypress/integration/visitorCanRequestQuotes.feature.js
@@ -17,22 +17,27 @@ describe("Visitor can request quotes", () => {
         cy.get('[name="email"]').type("peter@mail.com");
         cy.get('[name="telephone"]').type("0736123456");
         cy.get('[name="address"]').type("Address");
-        cy.get("[data-cy=installation-date]").type("within three months");
-        cy.get("[data-cy=property-type]").type("house");
-        cy.get("[data-cy=roof-slope]").type("15°");
-        cy.get("[data-cy=roof-type]").type("tiled");
-        cy.get("[data-cy=roof-direction]").type("north");
-        cy.get("[data-cy=gutter-height]").type("2.9m");
-        cy.get("[data-cy=roof-width]").type("8m");
-        cy.get("[data-cy=roof-length]").type("10m");
-        cy.get("[data-cy=fuse-size]").type("16A");
-        cy.get("[data-cy=energy-consumption]").type("25000kW");
-        cy.get("[data-cy=button]").contains("Submit").click();
       });
-      cy.get("[data-cy=message]").should(
-        "contain",
-        "Form successfully submitted."
-      );
+    });
+  });
+
+  context("Unsucessfully", () => {
+    beforeEach(() => {
+      cy.server();
+      cy.route({
+        method: "POST",
+        url: "http://localhost:3000/api/v1/quotes",
+        response: '{"message": "Address cant be blank"}',
+        status: "422",
+      });
+      cy.visit("/");
+      cy.get("[data-cy=button]").contains("Get up to three quotes").click();
+    });
+
+    it("with invalid credentials", () => {
+      cy.get('[name="name"]').type("Peter");
+      cy.get('[name="email"]').type("peter@mail.com");
+      cy.get('[name="telephone"]').type("0736123456");
     });
   });
 });
