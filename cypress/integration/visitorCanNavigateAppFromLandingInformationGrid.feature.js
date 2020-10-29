3e393884170360d70157f0862c4c76e3683a513c
Alex_Bellamy <alexlbellamy@gmail.com>
2020-10-29 17:12:55 +0100

    adding navigational specs from information grid component on landing page

---
+++ cypress/integration/visitorCanNavigateAppFromLandingInformationGrid.feature.js
@@ -0,0 +1,43 @@
+describe("Visitor can navigate app from landing information grid", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("visitor can navigate to get up to 3 quotes page", () => {
+    cy.get("#quotesDiv").click();
+    cy.get("[data-cy=title]").should("contain", "Get up to three quotes");
+    cy.get("#planetDiv").should("not.exist");
+    cy.get("#walletDiv").should("not.exist");
+    cy.get("#panelsWorkDiv").should("not.exist");
+  });
+
+  it("visitor can navigate environment page", () => {
+    cy.get("#planetDiv").click();
+    cy.get("[data-cy=title]").should(
+      "contain",
+      "How does this have an impact on the planet"
+    );
+    cy.get("#quotesDiv").should("not.exist");
+    cy.get("#walletDiv").should("not.exist");
+    cy.get("#panelsWorkDiv").should("not.exist");
+  });
+
+  it("visitor can navigate econonmy page", () => {
+    cy.get("#walletDiv").click();
+    cy.get("[data-cy=title]").should(
+      "contain",
+      "How does this have an impact on your wallet"
+    );
+    cy.get("#quotesDiv").should("not.exist");
+    cy.get("#planetDiv").should("not.exist");
+    cy.get("#panelsWorkDiv").should("not.exist");
+  });
+
+  it("visitor can how do solar panels work page", () => {
+    cy.get("#panelsWorkDiv").click();
+    cy.get("[data-cy=title]").should("contain", "How do solar panels work");
+    cy.get("#quotesDiv").should("not.exist");
+    cy.get("#planetDiv").should("not.exist");
+    cy.get("#walletDiv").should("not.exist");
+  });
+});
