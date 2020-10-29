2626de15de3a8d0419264ba599356a8cc929d8e3
felixoppongwillborg <fwlixw@gmail.com>
2020-10-29 17:35:20 +0100

    wrapped both icon, title and content inside a navlink created economics, environment and howItWorks component added routing in app.jsx

---
+++ src/components/HowItWorks.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HowItWorks = () => {
+  return (
+    <div>
+      <h1 data-cy="title">How do solar panels work</h1>
+    </div>
+  )
+}
+
+export default HowItWorks
--- src/components/LandingPageInfoSegment.jsx
+++ src/components/LandingPageInfoSegment.jsx
@@ -1,6 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Container, Grid, Icon } from "semantic-ui-react";
-
 
 const LandingPageInfoSegment = () => {
   return (
@@ -9,34 +9,48 @@ const LandingPageInfoSegment = () => {
         <Grid divided="vertically">
           <Grid.Row columns={3}>
             <Grid.Column textAlign="center">
-              <Icon id="threeQuotes" name="sun" size="massive" />
-              <h2 data-cy="title">Get up to 3 Quotes</h2>
+              <Link to={{ pathname: "/quotes" }} id="quotesDiv">
+                <Icon id="threeQuotes" name="sun" size="massive" />
+                <h2 data-cy="title">Get up to 3 Quotes</h2>
+              </Link>
             </Grid.Column>
           </Grid.Row>
           <Grid.Row columns={3}>
             <Grid.Column textAlign="center">
-              <Icon id="impactPlanet" name="sun" size="massive" />
-              <h2 data-cy="title">How does this have an impact on the planet</h2>
-              <p data-cy="content">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio,
-                eius? Maiores, impedit accusamus atque at quam{" "}
-              </p>
+              <Link to={{ pathname: "/environment" }} id="planetDiv">
+                <Icon id="impactPlanet" name="sun" size="massive" />
+                <h2 data-cy="title">
+                  How does this have an impact on the planet
+                </h2>
+                <p data-cy="content">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Odio, eius? Maiores, impedit accusamus atque at quam{" "}
+                </p>
+              </Link>
             </Grid.Column>
+
             <Grid.Column textAlign="center">
-              <Icon id="impactWallet" name="sun" size="massive" />
-              <h2 data-cy="title">How does this have an impact on your wallet</h2>
-              <p data-cy="content">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio,
-                eius? Maiores, impedit accusamus atque at{" "}
-              </p>
+              <Link to={{ pathname: "/economics" }} id="walletDiv">
+                <Icon id="impactWallet" name="sun" size="massive" />
+                <h2 data-cy="title">
+                  How does this have an impact on your wallet
+                </h2>
+                <p data-cy="content">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Odio, eius? Maiores, impedit accusamus atque at{" "}
+                </p>
+              </Link>
             </Grid.Column>
+
             <Grid.Column textAlign="center">
-              <Icon id="howPanelsWork" name="sun" size="massive" />
-              <h2 data-cy="title" >How do solar panels work?</h2>
-              <p data-cy="content">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio,
-                eius? Maiores, impedit accusamus atque{" "}
-              </p>
+              <Link to={{ pathname: "/howitworks" }} id="panelsWorkDiv">
+                <Icon id="howPanelsWork" name="sun" size="massive" />
+                <h2 data-cy="title">How do solar panels work?</h2>
+                <p data-cy="content">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Odio, eius? Maiores, impedit accusamus atque{" "}
+                </p>
+              </Link>
             </Grid.Column>
           </Grid.Row>
         </Grid>
