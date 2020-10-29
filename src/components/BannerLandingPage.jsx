81397cfd49ce4e1e5810aa17d90652366490cfb6
Alex_Bellamy <alexlbellamy@gmail.com>
2020-10-29 13:53:50 +0100

    creating LandingPageInfoSegment component, adding conditional to BannerLandingPage component to make it responsive to different devices and adding an alternative image for a mobile, styling in css file

--- src/components/BannerLandingPage.jsx
+++ src/components/BannerLandingPage.jsx
@@ -1,24 +1,67 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 import { Button, Icon } from "semantic-ui-react";
 
-const BannerLandingPage = () => {
+const BannerLandingPage = (props) => {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-device-width: 1224px)",
+  });
+  const isTabletOrMobileDevice = useMediaQuery({
+    query: "(max-device-width: 1224px)",
+  });
+
   return (
-    <div>
-      <div id="banner">
-        <img id="bannerImg" src="/images/BannerImage.jpg" alt="Top banner" />
-        <div id="bannerContent">
-          <p id="bannerInfoTxt">Tillsammans gör vi Sverige<br/>grönare med solceller <br/>
-          <mark id="subInfoTxt">Jämför upp till tre olika offerter<Icon id="angleDownIcon" name="angle down"/></mark>
-          </p>
-          <p id="bannerTxt">
-            We will donate <mark className="numbers">165.747 kr</mark>
-            <br /> to help save the planet<br/> <br/><Button id="bannerButton">Read more</Button>
-          </p>
-          
+    <>
+      {isDesktopOrLaptop && (
+        <div id="banner">
+          <img id="bannerImg" src="/images/BannerImage.jpg" alt="Top banner" />
+          <div id="bannerContent">
+            <p id="bannerInfoTxt">
+              Tillsammans gör vi Sverige
+              <br />
+              grönare med solceller <br />
+              <mark id="subInfoTxt">
+                Jämför upp till tre olika offerter
+                <Icon id="angleDownIcon" name="angle down" />
+              </mark>
+            </p>
+            <p id="bannerTxt">
+              We will donate <mark className="numbers">165.747 kr</mark>
+              <br /> to help save the planet
+              <br /> <br />
+              <Button id="bannerButton">Read more</Button>
+            </p>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+      {isTabletOrMobileDevice && (
+        <div id="banner">
+          <img
+            id="bannerImg"
+            src="/images/BannerImageMob.jpg"
+            alt="Top banner"
+          />
+          <div id="bannerContent">
+            <p id="bannerInfoTxt">
+              Tillsammans gör vi Sverige
+              <br />
+              grönare med solceller <br />
+              <mark id="subInfoTxt">
+                Jämför upp till tre olika offerter
+                <Icon id="angleDownIcon" name="angle down" />
+              </mark>
+            </p>
+            <p id="bannerTxt">
+              We will donate <mark className="numbers">165.747 kr</mark>
+              <br /> to help save the planet
+              <br /> <br />
+              <Button id="bannerButton">Read more</Button>
+            </p>
+          </div>
+        </div>
+      )}
+    </>
   );
-}
+};
 
 export default BannerLandingPage;
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import BannerLandingPage from "./BannerLandingPage";
+import LandingPageInfoSegment from "./LandingPageInfoSegment";
 
 const LandingPage = () => {
   return (
     <div>
-      <BannerLandingPage />
+      <BannerLandingPage /> 
+      <LandingPageInfoSegment />
     </div>
   );
 };
