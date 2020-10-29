e0f377515de6a4601651f14bb283a6a3bb766299
felixoppongwillborg <fwlixw@gmail.com>
2020-10-29 11:29:34 +0100

    added angle down icon and changed text and text position in banner on landingpage added a more descriptive text about company inside banner

--- src/components/BannerLandingPage.jsx
+++ src/components/BannerLandingPage.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Icon } from "semantic-ui-react";
 
-function BannerLandingPage() {
+const BannerLandingPage = () => {
   return (
     <div>
       <div id="banner">
         <img id="bannerImg" src="/images/BannerImage.jpg" alt="Top banner" />
         <div id="bannerContent">
+          <p id="bannerInfoTxt">Tillsammans gör vi Sverige<br/>grönare med solceller <br/>
+          <mark id="subInfoTxt">Jämför upp till tre olika offerter<Icon id="angleDownIcon" name="angle down"/></mark>
+          </p>
           <p id="bannerTxt">
             We will donate <mark className="numbers">165.747 kr</mark>
-            <br /> to help save the planet
+            <br /> to help save the planet<br/> <br/><Button id="bannerButton">Read more</Button>
           </p>
-          <Button id="bannerButton">Read more</Button>
+          
         </div>
       </div>
     </div>
