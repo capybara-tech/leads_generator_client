bf044696b5d9110d9675b35827b38b2571be55f5
felixoppongwillborg <fwlixw@gmail.com>
2020-10-30 11:30:41 +0100

    wrote test for visitor can request quotes with only mandatory fields and also for full quote request

--- cypress/integration/visitorCanRequestQuotes.feature.js
+++ cypress/integration/visitorCanRequestQuotes.feature.js
@@ -10,14 +10,38 @@ describe("Visitor can request quotes", () => {
       cy.visit("/");
       cy.get("[data-cy=button]").contains("Get up to 3 quotes").click();
     });
+  });
 
-    it("Visitor can create a quote request successfully", () => {
-      cy.get("[data-cy=quote-form]").within(() => {
-        cy.get('[name="name"]').type("Peter");
-        cy.get('[name="email"]').type("peter@mail.com");
-        cy.get('[name="telephone"]').type("0736123456");
-        cy.get('[name="address"]').type("Address");
-      });
+  it("Visitor can create a quote request successfully with just mandatory fields", () => {
+    cy.get("[data-cy=quote-form]").within(() => {
+      cy.get('[name="name"]').type("Peter");
+      cy.get('[name="email"]').type("peter@mail.com");
+      cy.get('[name="telephone"]').type("0736123456");
+      cy.get('[name="address"]').type("Address");
+      cy.get('[data-cy=message]').should("contain", "Thank you, from now on you just have to click some icons to get a more accurate quote or you can choose to submit right now")
+      cy.get('[data-cy=button]').contains("Submit").click()
+      cy.get('data-cy=message').should("contain", "Form successfully submitted.")
+    });
+  });
+
+  it("Visitor can create a full quote request successfully", () => {
+    cy.get("[data-cy=quote-form]").within(() => {
+      cy.get('[name="name"]').type("Peter");
+      cy.get('[name="email"]').type("peter@mail.com");
+      cy.get('[name="telephone"]').type("0736123456");
+      cy.get('[name="address"]').type("Address");
+      cy.get('#withinThreeMonthsIcon').click()
+      cy.get('#houseIcon').click()
+      cy.get('#slopeFlatIcon').click()
+      cy.get('#roofBrickIcon').click()
+      cy.get('#directionSouthIcon').click()
+      cy.get('[name="gutterHeight"]').type("4.2")
+      cy.get('[name="roofWidth"]').type("5.4")
+      cy.get('[name="roofLength"]').type("6.1")
+      cy.get('#fuse16AmpsIcon').click()
+      cy.get('[name="energyConsumption"]').type("10000")
+      cy.get('[data-cy=button]').contains("Submit").click()
+      cy.get('data-cy=message').should("contain", "Form successfully submitted.")
     });
   });
 
@@ -34,7 +58,7 @@ describe("Visitor can request quotes", () => {
       cy.get("[data-cy=button]").contains("Get up to 3 quotes").click();
     });
 
-    it("with invalid credentials", () => {
+    it("with missing address", () => {
       cy.get('[name="name"]').type("Peter");
       cy.get('[name="email"]').type("peter@mail.com");
       cy.get('[name="telephone"]').type("0736123456");
