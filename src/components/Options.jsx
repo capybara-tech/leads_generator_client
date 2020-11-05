9748e1b531459325bdaf5158e0f6a74075d0fc82
Alex_Bellamy <alexlbellamy@gmail.com>
2020-11-05 14:26:50 +0100

    amending Options component so that when an icon is clicked it updates the state to the value of the icon clicked, which avoids duplicate data input on the backend

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -7,14 +7,14 @@ const Options = () => {
     <div>
       <input type="hidden" name="installation_date" value={timeFrame} />
       <button
-        type="button"
-        onClick={() => setTimeFrame(timeFrame + "0-3 Months")}
+       type="button"
+        onClick={() => setTimeFrame("0-3 Months")}
       >
         <img src="/images/dummylogo.png" />
       </button>
       <button
         type="button"
-        onClick={() => setTimeFrame(timeFrame + "3-6 Months")}
+        onClick={() => setTimeFrame("3-6 Months")}
       >
         <img src="/images/dummylogo.png" />
       </button>
