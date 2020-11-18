37ac9de36815c74ba8816dbdaa173a8211913a3c
felixoppongwillborg <fwlixw@gmail.com>
2020-11-18 17:50:05 +0100

    created css file for installation date and added grid columns from semantic

--- src/components/additionalQuestions/InstallationDate.jsx
+++ src/components/additionalQuestions/InstallationDate.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import PropertyType from "./PropertyType";
 import { Link as Scrolllink } from "react-scroll";
+import { Grid } from "semantic-ui-react"
 
 const InstallationDate = () => {
   const [timeFrame, setTimeFrame] = useState("");
@@ -15,102 +16,112 @@ const InstallationDate = () => {
       </p>
       <h2>When are you looking for installation?</h2>
       <input type="hidden" name="installation_date" value={timeFrame} />
-      <Scrolllink
-        to="propertyType"
-        activeClass="active"
-        spy={true}
-        smooth={true}
-        offset={-30}
-        duration={1500}
-        onMouseDown={async (e) => {
-          setIndex(0);
-        }}
-      >
-        <button
-          className="installationIcons"
-          id="zeroToThreeMonthsIcon"
-          type="button"
-          onClick={() => setTimeFrame("0-3 Months")}
-        >
-          <img
-            className="installationIcons"
-            src="/images/dummylogo.png"
-            alt="0-3 months"
-          />
-        </button>{" "}
-      </Scrolllink>
-      <Scrolllink
-        to="propertyType"
-        activeClass="active"
-        spy={true}
-        smooth={true}
-        offset={-30}
-        duration={1500}
-        onMouseDown={async (e) => {
-          setIndex(0);
-        }}
-      >
-        <button
-          className="installationIcons"
-          id="threeToSixMonthsIcon"
-          type="button"
-          onClick={() => setTimeFrame("3-6 Months")}
-        >
-          <img
-            className="installationIcons"
-            src="/images/dummylogo.png"
-            alt="3-6 months"
-          />
-        </button>{" "}
-      </Scrolllink>
-      <Scrolllink
-        to="propertyType"
-        activeClass="active"
-        spy={true}
-        smooth={true}
-        offset={-30}
-        duration={1500}
-        onMouseDown={async (e) => {
-          setIndex(0);
-        }}
-      >
-        <button
-          className="installationIcons"
-          id="sixToTwelveMonthsIcon"
-          type="button"
-          onClick={() => setTimeFrame("6-12 Months")}
-        >
-          <img
-            className="installationIcons"
-            src="/images/dummylogo.png"
-            alt="6-12 months"
-          />
-        </button>{" "}
-      </Scrolllink>
-      <Scrolllink
-        to="propertyType"
-        activeClass="active"
-        spy={true}
-        smooth={true}
-        offset={-30}
-        duration={1500}
-        onMouseDown={async (e) => {
-          setIndex(0);
-        }}
-      >
-        <button
-          className="installationIcons"
-          id="notSure"
-          type="button"
-          onClick={() => setTimeFrame("Not sure")}
-        >
-          <img
-            className="installationIcons"
-            src="/images/dummylogo.png"
-            alt="Not sure"
-          />
-        </button>
-      </Scrolllink>{" "}
+      <Grid columns={4}>
+        <Grid.Column>
+          <Scrolllink
+            to="propertyType"
+            activeClass="active"
+            spy={true}
+            smooth={true}
+            offset={-30}
+            duration={1500}
+            onMouseDown={async (e) => {
+              setIndex(0);
+            }}
+          >
+            <button
+              className="installationIcons"
+              id="zeroToThreeMonthsIcon"
+              type="button"
+              onClick={() => setTimeFrame("0-3 Months")}
+            >
+              <img
+                className="installationIcons"
+                src="/images/dummylogo.png"
+                alt="0-3 months"
+              />
+            </button>{" "}
+          </Scrolllink>
+        </Grid.Column>
+        <Grid.Column>
+          <Scrolllink
+            to="propertyType"
+            activeClass="active"
+            spy={true}
+            smooth={true}
+            offset={-30}
+            duration={1500}
+            onMouseDown={async (e) => {
+              setIndex(0);
+            }}
+          >
+            <button
+              className="installationIcons"
+              id="threeToSixMonthsIcon"
+              type="button"
+              onClick={() => setTimeFrame("3-6 Months")}
+            >
+              <img
+                className="installationIcons"
+                src="/images/dummylogo.png"
+                alt="3-6 months"
+              />
+            </button>{" "}
+          </Scrolllink>
+        </Grid.Column>
+        <Grid.Column>
+          <Scrolllink
+            to="propertyType"
+            activeClass="active"
+            spy={true}
+            smooth={true}
+            offset={-30}
+            duration={1500}
+            onMouseDown={async (e) => {
+              setIndex(0);
+            }}
+          >
+            <button
+              className="installationIcons"
+              id="sixToTwelveMonthsIcon"
+              type="button"
+              onClick={() => setTimeFrame("6-12 Months")}
+            >
+              <img
+                className="installationIcons"
+                src="/images/dummylogo.png"
+                alt="6-12 months"
+              />
+            </button>{" "}
+          </Scrolllink>
+        </Grid.Column>
+        <Grid.Column>
+          <Scrolllink
+            to="propertyType"
+            activeClass="active"
+            spy={true}
+            smooth={true}
+            offset={-30}
+            duration={1500}
+            onMouseDown={async (e) => {
+              setIndex(0);
+            }}
+          >
+            <button
+              className="installationIcons"
+              id="notSure"
+              type="button"
+              onClick={() => setTimeFrame("Not sure")}
+            >
+              <img
+                className="installationIcons"
+                src="/images/dummylogo.png"
+                alt="Not sure"
+              />
+            </button>
+          </Scrolllink>{" "}
+        </Grid.Column>
+      </Grid>
       <br />
       <Scrolllink
         to="addressQuestion"
