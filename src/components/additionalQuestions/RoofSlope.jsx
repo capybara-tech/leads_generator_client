49951e6abb9463b80d508b3596f0c4dafa8e39b1
felixoppongwillborg <fwlixw@gmail.com>
2020-11-19 15:33:06 +0100

    created css file for roof slope, added basic styling for roof slope question, changed fonts positions, changed offset and image size added another color for Slope wording

--- src/components/additionalQuestions/RoofSlope.jsx
+++ src/components/additionalQuestions/RoofSlope.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import RoofType from "./RoofType";
+import "./RoofSlope.style.css";
 import { Link as Scrolllink } from "react-scroll";
+import { Grid } from "semantic-ui-react";
 
 const RoofSlope = () => {
   const [roofSlope, setRoofSlope] = useState("");
@@ -8,130 +10,170 @@ const RoofSlope = () => {
   const [index, setIndex] = useState("");
 
   return (
-    <div id="roofSlope">
-      <h2>What's the slope of your roof?</h2>
-      <input type="hidden" name="roof_slope" value={roofSlope} />
-      <Scrolllink
-        to="roofType"
-        activeClass="active"
-        spy={true}
-        smooth={true}
-        offset={-30}
-        duration={1500}
-        onMouseDown={async (e) => {
-          setIndex(0);
-        }}
-      >
-        <button
-          id="zeroToFiveDegreesIcon"
-          type="button"
-          onClick={() => setRoofSlope("0-5°")}
+    <>
+      <div id="roofSlope">
+        <input type="hidden" name="roof_slope" value={roofSlope} />
+        <Grid id="roofSlopeIcons">
+          <h2 id="questionForRoofSlope">
+            What's the <span id="slopeWord">slope</span> of your roof?
+          </h2>
+          <Grid.Row relaxed columns={4}>
+            <Grid.Column>
+              <Scrolllink
+                to="roofType"
+                activeClass="active"
+                spy={true}
+                smooth={true}
+                offset={-30}
+                duration={1500}
+                onMouseDown={async (e) => {
+                  setIndex(0);
+                }}
+              >
+                <button
+                  id="zeroToFiveDegreesIcon"
+                  type="button"
+                  onClick={() => setRoofSlope("0-5°")}
+                >
+                  <img
+                    className="imgRoofSlopeIcons"
+                    src="/images/dummylogo.png"
+                    alt="0-5°"
+                  />
+                </button>
+              </Scrolllink>
+            </Grid.Column>
+            <Grid.Column>
+              <Scrolllink
+                to="roofType"
+                activeClass="active"
+                spy={true}
+                smooth={true}
+                offset={-30}
+                duration={1500}
+                onMouseDown={async (e) => {
+                  setIndex(0);
+                }}
+              >
+                <button
+                  id="fiveToTwentyDegreesIcon"
+                  type="button"
+                  onClick={() => setRoofSlope("5-20°")}
+                >
+                  <img
+                    className="imgRoofSlopeIcons"
+                    src="/images/dummylogo.png"
+                    alt="5-20°"
+                  />
+                </button>
+              </Scrolllink>
+            </Grid.Column>
+            <Grid.Column>
+              <Scrolllink
+                to="roofType"
+                activeClass="active"
+                spy={true}
+                smooth={true}
+                offset={-30}
+                duration={1500}
+                onMouseDown={async (e) => {
+                  setIndex(0);
+                }}
+              >
+                <button
+                  id="twentyToFortyDegreesIcon"
+                  type="button"
+                  onClick={() => setRoofSlope("20-40°")}
+                >
+                  <img
+                    className="imgRoofSlopeIcons"
+                    src="/images/dummylogo.png"
+                    alt="20-40°"
+                  />
+                </button>
+              </Scrolllink>
+            </Grid.Column>
+            <Grid.Column>
+              <Scrolllink
+                to="roofType"
+                activeClass="active"
+                spy={true}
+                smooth={true}
+                offset={-30}
+                duration={1500}
+                onMouseDown={async (e) => {
+                  setIndex(0);
+                }}
+              >
+                <button
+                  id="fortyDegreesPlusIcon"
+                  type="button"
+                  onClick={() => setRoofSlope("40°+")}
+                >
+                  <img
+                    className="imgRoofSlopeIcons"
+                    src="/images/dummylogo.png"
+                    alt="40°+"
+                  />
+                </button>
+              </Scrolllink>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row relaxed columns={1}>
+            <Grid.Column>
+              <p id="exactRoofSlopeQ">If you know your exact roof slope please enter it below</p>
+              <input
+                id="fieldSlope"
+                component="input"
+                type="number"
+                placeholder="Exact roof slope"
+                onChange={(e) => setRoofSlope(e.target.value)}
+              ></input>
+
+              <Scrolllink
+                to="roofType"
+                activeClass="active"
+                spy={true}
+                smooth={true}
+                offset={-30}
+                duration={1500}
+                onMouseDown={async (e) => {
+                  setIndex(0);
+                }}
+              >
+                <button id="nextToRoofTypeQ">Next</button>
+              </Scrolllink>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+
+        <br />
+        <Scrolllink
+          to="propertyType"
+          activeClass="active"
+          spy={true}
+          smooth={true}
+          offset={-30}
+          duration={1500}
         >
-          <img src="/images/dummylogo.png" alt="0-5°" />
-        </button>
-      </Scrolllink>
-      <Scrolllink
-        to="roofType"
-        activeClass="active"
-        spy={true}
-        smooth={true}
-        offset={-30}
-        duration={1500}
-        onMouseDown={async (e) => {
-          setIndex(0);
-        }}
-      >
-        <button
-          id="fiveToTwentyDegreesIcon"
-          type="button"
-          onClick={() => setRoofSlope("5-20°")}
+          <button id="backToPropertyTypeQ">Back</button>
+        </Scrolllink>
+        <Scrolllink
+          to="roofType"
+          activeClass="active"
+          spy={true}
+          smooth={true}
+          offset={-30}
+          duration={1500}
+          onMouseDown={async (e) => {
+            setIndex(0);
+          }}
         >
-          <img src="/images/dummylogo.png" alt="5-20°" />
-        </button>
-      </Scrolllink>
-      <Scrolllink
-        to="roofType"
-        activeClass="active"
-        spy={true}
-        smooth={true}
-        offset={-30}
-        duration={1500}
-        onMouseDown={async (e) => {
-          setIndex(0);
-        }}
-      >
-        <button
-          id="twentyToFortyDegreesIcon"
-          type="button"
-          onClick={() => setRoofSlope("20-40°")}
-        >
-          <img src="/images/dummylogo.png" alt="20-40°" />
-        </button>
-      </Scrolllink>
-      <Scrolllink
-        to="roofType"
-        activeClass="active"
-        spy={true}
-        smooth={true}
-        offset={-30}
-        duration={1500}
-        onMouseDown={async (e) => {
-          setIndex(0);
-        }}
-      >
-        <button
-          id="fortyDegreesPlusIcon"
-          type="button"
-          onClick={() => setRoofSlope("40°+")}
-        >
-          <img src="/images/dummylogo.png" alt="40°+" />
-        </button>
-      </Scrolllink>
-      <input
-        component="input"
-        type="number"
-        placeholder="Exact roof slope"
-        onChange={(e) => setRoofSlope(e.target.value)}
-      ></input>
-      <Scrolllink
-        to="roofType"
-        activeClass="active"
-        spy={true}
-        smooth={true}
-        offset={-30}
-        duration={1500}
-        onMouseDown={async (e) => {
-          setIndex(0);
-        }}
-      >
-        Next
-      </Scrolllink>
-      <br />
-      <Scrolllink
-        to="propertyType"
-        activeClass="active"
-        spy={true}
-        smooth={true}
-        offset={-30}
-        duration={1500}
-      >
-        Back
-      </Scrolllink>
-      <Scrolllink
-        to="roofType"
-        activeClass="active"
-        spy={true}
-        smooth={true}
-        offset={-30}
-        duration={1500}
-        onMouseDown={async (e) => {
-          setIndex(0);
-        }}
-      >
-        Skip
-      </Scrolllink>
+          <button id="skipToRoofTypeQ">Skip</button>
+        </Scrolllink>
+      </div>
       {components[index]}
-    </div>
+    </>
   );
 };
 
