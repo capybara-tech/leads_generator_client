d550b8924a87af7f4d162df7fb5d7effa49454e3
felixoppongwillborg <fwlixw@gmail.com>
2020-11-25 12:30:08 +0100

    created Roof dimenions css file and added basic styling changed fonts and fontsizes together with backgroundcolor etc

--- src/components/additionalQuestions/RoofDimensions.jsx
+++ src/components/additionalQuestions/RoofDimensions.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Field } from "react-final-form";
 import Electricity from "./Electricity";
+import "./RoofDimensions.style.css"
 import { Link as Scrolllink } from "react-scroll";
 
 const RoofDimensions = () => {
@@ -8,66 +9,73 @@ const RoofDimensions = () => {
   const [index, setIndex] = useState("");
 
   return (
-    <div id="roofDimensions">
-      <h2>What's your roof dimension?</h2>
-      <Field
-        name="gutter_height"
-        component="input"
-        type="number"
-        placeholder="Gutter height"
-      />
-      <Field
-        name="roof_width"
-        component="input"
-        type="number"
-        placeholder="Roof width"
-      />
-      <Field
-        name="roof_length"
-        component="input"
-        type="number"
-        placeholder="Roof length"
-      />
-      <Scrolllink
-        id="nextToElectricity"
-        to="electricity"
-        activeClass="active"
-        spy={true}
-        smooth={true}
-        offset={-30}
-        duration={1500}
-        onMouseDown={async (e) => {
-          setIndex(0);
-        }}
-      >
-        Next
-      </Scrolllink>
-      <br />
-      <Scrolllink
-        to="roofDirection"
-        activeClass="active"
-        spy={true}
-        smooth={true}
-        offset={-30}
-        duration={1500}
-      >
-        Back
-      </Scrolllink>
-      <Scrolllink
-        to="electricity"
-        activeClass="active"
-        spy={true}
-        smooth={true}
-        offset={-30}
-        duration={1500}
-        onMouseDown={async (e) => {
-          setIndex(0);
-        }}
-      >
-        Skip
-      </Scrolllink>
+    <>
+      <div id="roofDimensions">
+    <div id="questionForRoofDimensions">
+        <h3 id="questionAskingForRoofDimensions"> What's your roof <span id="dimesionWord">dimension</span>?</h3>
+        <Field
+        id="fieldGutterHeight"
+          name="gutter_height"
+          component="input"
+          type="number"
+          placeholder="Gutter height"
+        />
+        <Field
+        id="fieldRoofWidth"
+          name="roof_width"
+          component="input"
+          type="number"
+          placeholder="Roof width"
+        />
+        <Field
+        id="fieldRoofLength"
+          name="roof_length"
+          component="input"
+          type="number"
+          placeholder="Roof length"
+        />
+        </div>
+        <Scrolllink
+          id="nextToElectricity"
+          to="electricity"
+          activeClass="active"
+          spy={true}
+          smooth={true}
+          offset={-30}
+          duration={1500}
+          onMouseDown={async (e) => {
+            setIndex(0);
+          }}
+        >
+          <button id="nextToElectricityQ">Next</button>
+        </Scrolllink>
+        <br />
+        <Scrolllink
+          to="roofDirection"
+          activeClass="active"
+          spy={true}
+          smooth={true}
+          offset={-30}
+          duration={1500}
+        >
+          <button id="backToRoofDirectionQ">Back</button>
+        </Scrolllink>
+        <Scrolllink
+          to="electricity"
+          activeClass="active"
+          spy={true}
+          smooth={true}
+          offset={-30}
+          duration={1500}
+          onMouseDown={async (e) => {
+            setIndex(0);
+          }}
+        >
+          <button id="skipToElectricityQ">Skip</button>
+        </Scrolllink>
+      </div>
       {components[index]}
-    </div>
+    </>
   );
 };
 
