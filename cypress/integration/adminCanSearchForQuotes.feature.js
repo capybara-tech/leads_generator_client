c3bbc71e0e7e23c2d87afec592ab8b00c7596f18
Alex_Bellamy <alexlbellamy@gmail.com>
2020-11-25 16:15:44 +0100

    writing initial tests for admin can search for quotes and creating a quotes index json file

---
+++ cypress/integration/adminCanSearchForQuotes.feature.js
@@ -0,0 +1,37 @@
+describe("Admin can see search for quotes", () => {
+  context("successfully", () => {
+    beforeEach(() => {
+      cy.server();
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/v1/admin/quotes",
+        response: "fixture:quotes_index.json",
+      });
+      cy.route({
+        method: "POST",
+        url: "http://localhost:3000/api/v1/auth/sign_in",
+        response: "fixture:registration_response.json",
+      });
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/v1/auth/**",
+        response: "fixture:registration_response.json",
+      });
+      cy.visit("/");
+      cy.get("[data-cy=button]").contains("Admin").click({ force: true });
+      cy.get("[data-cy=login-form]").within(() => {
+        cy.get("[data-cy=email]").type("admin@mail.com");
+        cy.get("[data-cy=password]").type("password");
+        cy.get("[data-cy=button]").contains("Login").click();
+      });
+      cy.get("[data-cy=button]").contains("Quotes").click();
+    });
+
+    it("Admin can search for specific quotes", () => {
+      cy.get("[data-cy=search]").type("Liljevalchsgatan 24");
+      cy.get("[data-cy=name]").should("contain", "Peter Andrews");
+      cy.get("[data-cy=email]").should("contain", "peter123@mail.com");
+      cy.get("[data-cy=telephone]").should("contain", "0787890767");
+    });
+  });
+});
