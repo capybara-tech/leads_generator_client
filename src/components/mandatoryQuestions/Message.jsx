8b206ef6b3b6fd6e134f460008105a1978758b14
felixoppongwillborg <fwlixw@gmail.com>
2020-12-10 17:01:07 +0100

    added conditional in message for success message and error message

--- src/components/mandatoryQuestions/Message.jsx
+++ src/components/mandatoryQuestions/Message.jsx
@@ -1,10 +1,30 @@
 import React from "react";
-import {Modal} from "semantic-ui-react"
+import { Modal } from "semantic-ui-react";
 
 const Message = ({ message }) => {
   return (
     <div>
-      <Modal open={message}>{message}</Modal>
+      {message.length > 28 ? (
+        <>
+          {/* error message */}
+          <Modal basic open={message}>
+            <Modal.Content>
+              <h1>error</h1>
+              <p>{message}</p>
+            </Modal.Content>
+          </Modal>
+        </>
+      ) : (
+        <>
+          {/* success message */}
+          <Modal basic open={message}>
+            <Modal.Content>
+              <h1>success</h1>
+              <p>{message}</p>
+            </Modal.Content>
+          </Modal>
+        </>
+      )}
     </div>
   );
 };
