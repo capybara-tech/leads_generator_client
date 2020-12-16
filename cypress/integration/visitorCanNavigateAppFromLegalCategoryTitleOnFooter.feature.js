2d2483c598ed9afd731fee257c6d12ef2eee14bd
Alex_Bellamy <alexlbellamy@gmail.com>
2020-12-16 15:12:44 +0100

    writing specs visitor can navigate app from legal category title on footer

---
+++ cypress/integration/visitorCanNavigateAppFromLegalCategoryTitleOnFooter.feature.js
@@ -0,0 +1,101 @@
+describe("Visitor can navigate app from legal category title on the footer", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("to Terms and conditions", () => {
+    cy.get("[data-cy=footer]").within(() => {
+      cy.get("[data-cy=button]").contains("Terms and conditions").click();
+    });
+    cy.get("[data-cy=title]").should("contain", "Terms and conditions");
+    cy.get("[data-cy=title]")
+      .contains("How solar works?")
+      .should("not.be.visible");
+    cy.get("[data-cy=title]").contains("About us").should("not.be.visible");
+    cy.get("[data-cy=title]")
+      .contains("The SolSpana way")
+      .should("not.be.visible");
+    cy.get("[data-cy=title]")
+      .contains("How does this have an impact on the planet")
+      .should("not.be.visible");
+    cy.get("[data-cy=title]")
+      .contains("How does this have an impact on your wallet")
+      .should("not.be.visible");
+    cy.get("[data-cy=title]")
+      .contains("Become a partner")
+      .should("not.be.visible");
+    cy.get("[data-cy=title]")
+      .contains("My name's Felix, what's yours?")
+      .should("not.be.visible");
+    cy.get("[data-cy=title]")
+      .contains("Privacy policy")
+      .should("not.be.visible");
+    cy.get("[data-cy=title]").contains("Cookies").should("not.be.visible");
+    cy.get("[data-cy=title]").contains("Contact us").should("not.be.visible");
+    cy.get("[data-cy=title]").contains("FAQs").should("not.be.visible");
+  });
+
+  it("to Privacy policy", () => {
+    cy.get("[data-cy=footer]").within(() => {
+      cy.get("[data-cy=button]").contains("Privacy policy").click();
+    });
+    cy.get("[data-cy=title]").should("contain", "Privacy policy");
+    cy.get("[data-cy=title]")
+      .contains("How solar works?")
+      .should("not.be.visible");
+    cy.get("[data-cy=title]").contains("About us").should("not.be.visible");
+    cy.get("[data-cy=title]")
+      .contains("The SolSpana way")
+      .should("not.be.visible");
+    cy.get("[data-cy=title]")
+      .contains("How does this have an impact on the planet")
+      .should("not.be.visible");
+    cy.get("[data-cy=title]")
+      .contains("How does this have an impact on your wallet")
+      .should("not.be.visible");
+    cy.get("[data-cy=title]")
+      .contains("Become a partner")
+      .should("not.be.visible");
+    cy.get("[data-cy=title]")
+      .contains("My name's Felix, what's yours?")
+      .should("not.be.visible");
+    cy.get("[data-cy=title]")
+      .contains("Terms and conditions")
+      .should("not.be.visible");
+    cy.get("[data-cy=title]").contains("Cookies").should("not.be.visible");
+    cy.get("[data-cy=title]").contains("Contact us").should("not.be.visible");
+    cy.get("[data-cy=title]").contains("FAQs").should("not.be.visible");
+  });
+
+  it("to Cookies", () => {
+    cy.get("[data-cy=footer]").within(() => {
+      cy.get("[data-cy=button]").contains("Cookies").click();
+    });
+    cy.get("[data-cy=title]").should("contain", "Cookies");
+    cy.get("[data-cy=title]")
+      .contains("How solar works?")
+      .should("not.be.visible");
+    cy.get("[data-cy=title]").contains("About us").should("not.be.visible");
+    cy.get("[data-cy=title]")
+      .contains("The SolSpana way")
+      .should("not.be.visible");
+    cy.get("[data-cy=title]")
+      .contains("How does this have an impact on the planet")
+      .should("not.be.visible");
+    cy.get("[data-cy=title]")
+      .contains("How does this have an impact on your wallet")
+      .should("not.be.visible");
+    cy.get("[data-cy=title]")
+      .contains("Become a partner")
+      .should("not.be.visible");
+    cy.get("[data-cy=title]")
+      .contains("My name's Felix, what's yours?")
+      .should("not.be.visible");
+    cy.get("[data-cy=title]")
+      .contains("Terms and conditions")
+      .should("not.be.visible");
+    cy.get("[data-cy=title]").contains("Cookies").should("not.be.visible");
+    cy.get("[data-cy=title]").contains("Contact us").should("not.be.visible");
+    cy.get("[data-cy=title]").contains("FAQs").should("not.be.visible");
+  });
+});
