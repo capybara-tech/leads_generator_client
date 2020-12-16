60b171e4867f0fdb653156484014ff3db0cdad40
Alex_Bellamy <alexlbellamy@gmail.com>
2020-12-16 02:04:26 +0100

    adding titles for the different categories of links in the footer and incorporating a semantic container and grid into the footer component

--- src/components/generalPageContent/Footer.jsx
+++ src/components/generalPageContent/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
+import { Container, Grid } from "semantic-ui-react";
 
 const Footer = (props) => {
   if (props.location.pathname === "/adminhomepage") {
@@ -7,31 +8,51 @@ const Footer = (props) => {
   }
   return (
     <div data-cy="footer" className="footer">
-      <h3>Discover</h3>
-      <Link data-cy="button" as={Link} to={{ pathname: "/aboutus" }}>
-        About us
-      </Link>{" "}
-      <br />
-      <Link data-cy="button" as={Link} to={{ pathname: "/howitworks" }}>
-        How SOLSPANA works
-      </Link>{" "}
-      <br />
-      <Link data-cy="button" as={Link} to={{ pathname: "/environment" }}>
-        Impact on planet
-      </Link>{" "}
-      |{" "}
-      <Link data-cy="button" as={Link} to={{ pathname: "/economics" }}>
-        Impact on wallet
-      </Link>{" "}
-      <br />
-      <Link data-cy="button" as={Link} to={{ pathname: "/partner" }}>
-        Become a partner
-      </Link>{" "}
-      <br />
-      <Link data-cy="button" as={Link} to={{ pathname: "/quotes" }}>
-        Get up to 3 quotes
-      </Link>{" "}
-      <br />
+      <Container>
+        <Grid divided="vertically">
+          <Grid.Row columns={4}>
+            <Grid.Column textAlign="center">
+              <h3>Solar</h3>
+            </Grid.Column>
+            <Grid.Column textAlign="center">
+              <h3>Discover</h3>
+              <Link data-cy="button" as={Link} to={{ pathname: "/aboutus" }}>
+                About us
+              </Link>{" "}
+              <br />
+              <Link data-cy="button" as={Link} to={{ pathname: "/howitworks" }}>
+                How SOLSPANA works
+              </Link>{" "}
+              <br />
+              <Link
+                data-cy="button"
+                as={Link}
+                to={{ pathname: "/environment" }}
+              >
+                Impact on planet
+              </Link>{" "}
+              |{" "}
+              <Link data-cy="button" as={Link} to={{ pathname: "/economics" }}>
+                Impact on wallet
+              </Link>{" "}
+              <br />
+              <Link data-cy="button" as={Link} to={{ pathname: "/partner" }}>
+                Become a partner
+              </Link>{" "}
+              <br />
+              <Link data-cy="button" as={Link} to={{ pathname: "/quotes" }}>
+                Get up to 3 quotes
+              </Link>{" "}
+            </Grid.Column>
+            <Grid.Column textAlign="center">
+              <h3>Boring stuff</h3>
+            </Grid.Column>
+            <Grid.Column textAlign="center">
+              <h3>Help</h3>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
       <p>
         {" "}
         &copy;{new Date().getFullYear()} CAPYBARA TECH AB | All rights reserved
