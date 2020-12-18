8a2f2e63aea08d6cb1e12c410847fc8a1c194560
felixoppongwillborg <fwlixw@gmail.com>
2020-12-18 16:25:59 +0100

    added grid skeleton for icons and text next to them in a div that sits to the right of page, created a div that sits to left with one icon in it changed height of footer

--- src/components/generalPageContent/LandingPageInfoSegment.jsx
+++ src/components/generalPageContent/LandingPageInfoSegment.jsx
@@ -1,79 +1,88 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container, Grid, Icon } from "semantic-ui-react";
+import { Icon, Grid } from "semantic-ui-react";
+import "./LandingPageInfoSegment.style.css";
 
 const LandingPageInfoSegment = () => {
   return (
     <div id="infoSegment">
-      <Container id="landingPageInfoSegment">
-        <Grid divided="vertically">
-          <Grid.Row columns={3}>
-            <Grid.Column textAlign="center">
-              <Link
-                to={{ pathname: "/quotes" }}
-                id="quotesDiv"
-                className="infoSegmentIcons"
-              >
-                <Icon
-                  id="threeQuotes"
-                  className="infoSegmentIcons"
-                  name="sun"
-                  size="massive"
-                />
-                <h2 data-cy="title">Get up to 3 Quotes</h2>
-              </Link>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row columns={3}>
-            <Grid.Column textAlign="center">
+      <div id="midPageIcon">
+        <Link
+          to={{ pathname: "/quotes" }}
+          id="quotesDiv"
+          className="infoSegmentIcons"
+        >
+          <Icon
+            id="threeQuotes"
+            className="infoSegmentIcons"
+            name="sun"
+            size="massive"
+          />
+          <h3 data-cy="title">Get up to 3 Quotes</h3>
+        </Link>
+      </div>
+      <div id="boxWithIcons">
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={4}>
               <Link
                 to={{ pathname: "/environment" }}
                 id="planetDiv"
                 className="infoSegmentIcons"
               >
                 <Icon id="impactPlanet" name="sun" size="massive" />
-                <h2 data-cy="title">
-                  How does this have an impact on the planet
-                </h2>
-                <p data-cy="content">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Odio, eius? Maiores, impedit accusamus atque at quam{" "}
-                </p>
               </Link>
             </Grid.Column>
-            <Grid.Column textAlign="center">
+            <Grid.Column width={12}>
+              <h3 data-cy="title">
+                How does this have an impact on the planet
+              </h3>
+              <p data-cy="content">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio,
+                eius? Maiores, impedit accusamus atque at quam{" "}
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={4}>
               <Link
                 to={{ pathname: "/economics" }}
                 id="walletDiv"
                 className="infoSegmentIcons"
               >
                 <Icon id="impactWallet" name="sun" size="massive" />
-                <h2 data-cy="title">
-                  How does this have an impact on your wallet
-                </h2>
-                <p data-cy="content">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Odio, eius? Maiores, impedit accusamus atque at{" "}
-                </p>
               </Link>
             </Grid.Column>
-            <Grid.Column textAlign="center">
+            <Grid.Column width={12}>
+              <h3 data-cy="title">
+                How does this have an impact on your wallet
+              </h3>
+              <p data-cy="content">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio,
+                eius? Maiores, impedit accusamus atque at{" "}
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={4}>
               <Link
                 to={{ pathname: "/howsolarworks" }}
                 id="panelsWorkDiv"
                 className="infoSegmentIcons"
               >
                 <Icon id="howPanelsWork" name="sun" size="massive" />
-                <h2 data-cy="title">How do solar panels work?</h2>
-                <p data-cy="content">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Odio, eius? Maiores, impedit accusamus atque{" "}
-                </p>
               </Link>
+            </Grid.Column>
+            <Grid.Column width={12}>
+              <h3 data-cy="title">How do solar panels work?</h3>
+              <p data-cy="content">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio,
+                eius? Maiores, impedit accusamus atque{" "}
+              </p>
             </Grid.Column>
           </Grid.Row>
         </Grid>
-      </Container>
+      </div>
     </div>
   );
 };
