062646d92d3cd297155f3739fb210398fceb0473
Alex_Bellamy <alexlbellamy@gmail.com>
2021-01-08 13:16:43 +0100

    amending title of becomeapartner page, styling the same and adding window scroll to top link once page renders

--- src/components/generalPageContent/BecomeAPartner.jsx
+++ src/components/generalPageContent/BecomeAPartner.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./BecomeAPartner.style.css";
 
 const BecomeAPartner = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
   return (
     <div id="becomeAPartnerContent">
-      <h1 data-cy="title">Become a partner</h1>
-      <h3>Site currently under construction...</h3>
+      <h1 id="title" data-cy="title">Join #TEAMSOLSPANA</h1>
     </div>
   );
 };
