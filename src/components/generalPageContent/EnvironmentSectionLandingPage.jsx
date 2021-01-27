9b8ac7ea876ea3262cf00ead8f885c831f83f876
felixoppongwillborg <fwlixw@gmail.com>
2021-01-27 18:59:25 +0100

    added image of tree in environmental sectionchanged parallax size to cover to make it responsive

--- src/components/generalPageContent/EnvironmentSectionLandingPage.jsx
+++ src/components/generalPageContent/EnvironmentSectionLandingPage.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button } from 'semantic-ui-react'
-import './EnvironmentalSectionLandingPage.style.css'
-
+import { Button } from "semantic-ui-react";
+import "./EnvironmentalSectionLandingPage.style.css";
 
 const EnvironmentSectionLandingPage = () => {
   return (
     <div id="environmentSection">
       <div id="environmentalContentLandingPage">
-        <p>We love our planet so much as such we donate a massive 15% of our net profits to other green initiatives</p>
-      <Button
-        id="bannerButton"
-        data-cy="button"
-        as={Link}
-        to={{ pathname: "/aboutus" }}
-      >
-        Read more
-      </Button>
+        <p>
+          We love our planet so much as such we donate a massive 15% of our net
+          profits to other green initiatives
+        </p>
+        <Button
+          id="bannerButton"
+          data-cy="button"
+          as={Link}
+          to={{ pathname: "/aboutus" }}
+        >
+          Read more
+        </Button>
       </div>
+      <div>
+     <img id="heroTree" src="/images/HeroTree1.png" alt="tree" />
+   </div>
     </div>
   );
 };
