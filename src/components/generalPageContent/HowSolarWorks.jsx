3c937b91bd8ee0752e4ed39c4df68e82ebdd6068
Alex_Bellamy <alexlbellamy@gmail.com>
2021-01-27 22:12:05 +0100

    adding titles to how solar works component, adding first card and styling the same, updating sitewording.json

--- src/components/generalPageContent/HowSolarWorks.jsx
+++ src/components/generalPageContent/HowSolarWorks.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import "./HowSolarWorks.style.css";
+import { Card, Icon } from "semantic-ui-react";
 
 const HowSolarWorks = () => {
   useEffect(() => {
@@ -8,8 +9,37 @@ const HowSolarWorks = () => {
 
   return (
     <div id="howSolarWorksContent">
-      <h1 data-cy="title">How does solar work?</h1>
-      <h3>Site currently under construction...</h3>
+      <h1 data-cy="title" id="howSolarWorksTitle">
+        How do solar panels actually work?
+      </h1>
+      <h3 id="howSolarWorksSubTitle">
+        Lets keep it simple, like the process itself:
+      </h3>
+      <div id="howSolarWorksCards">
+        <Card.Group>
+          <Card id="howSolarWorksCard1">
+            <Card.Content>
+              <Card.Header id="howSolarWorksNumber1">01</Card.Header>
+              <br />
+              <Card.Header id="howSolarWorksCardTitle1">
+                Daylight
+              </Card.Header>{" "}
+              <br />
+              <Card.Description id="howSolarWorksCardContent1">
+                Not only sunshine, daylight hits the solar photovoltaic cells in
+                the panels.
+              </Card.Description> <br /> <br /> <br />
+              <Card.Description>
+                <Icon
+                  id="howSolarWorksArrow1"
+                  size="huge"
+                  name="arrow circle right"
+                />
+              </Card.Description>
+            </Card.Content>
+          </Card>
+        </Card.Group>
+      </div>
     </div>
   );
 };
