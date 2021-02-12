dccf2feb1c46157c5033a3190d6419ecfd3cf8d8
felixoppongwillborg <fwlixw@gmail.com>
2021-02-12 11:24:26 +0100

    created mobile version for telephone question in quotes form

--- src/components/mandatoryQuestions/TelephoneQuestion.jsx
+++ src/components/mandatoryQuestions/TelephoneQuestion.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./TelephoneQuestion.style.css";
 import { Field } from "react-final-form";
 import { Link as Scrolllink } from "react-scroll";
+import Media from "react-media";
 import { Popup, Icon } from "semantic-ui-react";
 
 const normaliseTelephone = (value) => {
@@ -12,60 +13,149 @@ const normaliseTelephone = (value) => {
 
 const TelephoneQuestion = () => {
   return (
-    <div id="telephoneQuestion">
-      <div id="telephoneQuestionDiv">
-        <div id="questionForTelephone">
-          <p id="questionAskingForTelephone">
-            <img
-              id="nameQuestionImage"
-              src="/images/questionLogo.png"
-              alt="telephone question"
-            />{" "}
-            {""}
-            Great, and the best number to contact you on?
-          </p>
-          <Field
-            id="fieldTelephone"
-            name="telephone"
-            component="input"
-            type="text"
-            placeholder="Telephone"
-            parse={normaliseTelephone}
-          />
-          <br />
-        </div>
+    <>
+      <div>
+        <Media
+          queries={{
+            mobile: "(max-width: 599px)",
+            desktop: "(min-width: 600px)",
+          }}
+        >
+          {(matches) => (
+            <>
+              {matches.mobile && (
+                <div id="telephoneQuestion">
+                  <div id="telephoneQuestionDiv">
+                    <div id="questionForTelephone">
+                      <p id="questionAskingForTelephone">
+                        <img
+                          id="nameQuestionImage"
+                          src="/images/questionLogo.png"
+                          alt="telephone question"
+                        />{" "}
+                        {""}
+                        Great, and the best number to contact you on?
+                      </p>
+                      <Field
+                        id="fieldTelephoneMobile"
+                        name="telephone"
+                        component="input"
+                        type="text"
+                        placeholder="Telephone"
+                        parse={normaliseTelephone}
+                      />
+                      <br />
+                    </div>
+                  </div>
+                  <Scrolllink
+                    to="emailQuestion"
+                    activeClass="active"
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={1500}
+                  >
+                    <Popup
+                      trigger={
+                        <Icon size="big" name="angle left" id="backToEmailQ" />
+                      }
+                      content="Back to previous question"
+                      inverted
+                      position="bottom center"
+                    />
+                  </Scrolllink>
+                  <Scrolllink
+                    to="addressQuestion"
+                    activeClass="active"
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={1500}
+                  >
+                    <Popup
+                      trigger={
+                        <Icon
+                          size="big"
+                          name="angle right"
+                          id="nextToAddressQ"
+                        />
+                      }
+                      content="Next question"
+                      inverted
+                      position="bottom center"
+                    />
+                  </Scrolllink>
+                </div>
+              )}
+              {matches.desktop && (
+                <div id="telephoneQuestion">
+                  <div id="telephoneQuestionDiv">
+                    <div id="questionForTelephone">
+                      <p id="questionAskingForTelephone">
+                        <img
+                          id="nameQuestionImage"
+                          src="/images/questionLogo.png"
+                          alt="telephone question"
+                        />{" "}
+                        {""}
+                        Great, and the best number to contact you on?
+                      </p>
+                      <Field
+                        id="fieldTelephone"
+                        name="telephone"
+                        component="input"
+                        type="text"
+                        placeholder="Telephone"
+                        parse={normaliseTelephone}
+                      />
+                      <br />
+                    </div>
+                  </div>
+                  <Scrolllink
+                    to="emailQuestion"
+                    activeClass="active"
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={1500}
+                  >
+                    <Popup
+                      trigger={
+                        <Icon size="big" name="angle left" id="backToEmailQ" />
+                      }
+                      content="Back to previous question"
+                      inverted
+                      position="bottom center"
+                    />
+                  </Scrolllink>
+                  <Scrolllink
+                    to="addressQuestion"
+                    activeClass="active"
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={1500}
+                  >
+                    <Popup
+                      trigger={
+                        <Icon
+                          size="big"
+                          name="angle right"
+                          id="nextToAddressQ"
+                        />
+                      }
+                      content="Next question"
+                      inverted
+                      position="bottom center"
+                    />
+                  </Scrolllink>
+                </div>
+              )}
+            </>
+          )}
+        </Media>
       </div>
-      <Scrolllink
-        to="emailQuestion"
-        activeClass="active"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={1500}
-      >
-        <Popup
-          trigger={<Icon size="big" name="angle left" id="backToEmailQ" />}
-          content="Back to previous question"
-          inverted
-          position="bottom center"
-        />
-      </Scrolllink>
-      <Scrolllink
-        to="addressQuestion"
-        activeClass="active"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={1500}
-      >
-        <Popup
-          trigger={<Icon size="big" name="angle right" id="nextToAddressQ" />}
-          content="Next question"
-          inverted
-          position="bottom center"
-        />
-      </Scrolllink>
-    </div>
+    </>
   );
 };
 
