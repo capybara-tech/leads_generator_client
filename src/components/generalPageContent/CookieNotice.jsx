5b00b4e6ebdffa452557e3c9dfb97083dbb0910b
Alex_Bellamy <alexlbellamy@gmail.com>
2021-02-18 17:01:43 +0100

    adding link on cookienotice component to cookie policy page

--- src/components/generalPageContent/CookieNotice.jsx
+++ src/components/generalPageContent/CookieNotice.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import CookieConsent from "react-cookie-consent";
+import { Link } from "react-router-dom";
 
 const CookieNotice = () => {
   return (
     <div>
       <CookieConsent
-      
         location="bottom"
         buttonText="Understood"
         expires={150}
       >
         This website uses cookies to enhance your experience. Learn more:{" "}
+        <div>
+          <Link data-cy="content" to="/cookies">
+            terms
+          </Link>
+        </div>
       </CookieConsent>
     </div>
   );
