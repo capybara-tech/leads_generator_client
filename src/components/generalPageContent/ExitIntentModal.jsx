b6c9828a290997c2ffdb4b4460dc6a0190262622
Alex_Bellamy <alexlbellamy@gmail.com>
2021-03-02 23:12:02 +0100

    utilise semantic ui modal in exitexitmodal component, add card and populate with content, style the same

--- src/components/generalPageContent/ExitIntentModal.jsx
+++ src/components/generalPageContent/ExitIntentModal.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import "./ExitIntentModal.style.css";
+import { Modal, Card, Icon } from "semantic-ui-react";
 
 const ExitIntentModal = (props) => {
   const [show, setShow] = useState(props.show);
@@ -13,8 +15,39 @@ const ExitIntentModal = (props) => {
 
   return show ? (
     <div>
-      <h1>pop up message</h1>
-      <button onClick={close}>close</button>
+      <Modal basic open={props.show}>
+        <Modal.Content>
+          <Card id="exitIntentCard" centered={true}>
+            <Card.Content id="exitIntentContent">
+              <Card.Header id="exitIntentHeader">
+                Don't leave yet!
+                <br />
+              </Card.Header>
+              <Card.Description id="exitIntentContent">
+                Obtain quotes now to be entered into our{" "}
+                <span id="exitIntentHighlightedWord">free £100</span> cash prize
+                draw*
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <Card.Description id="exitIntentDisclaimer">
+                  We ask only those who are genuinely interested in solar to
+                  request quotes
+                </Card.Description>
+                <Card.Description id="exitIntentSmallPrint">
+                  *terms apply, see our terms and conditions
+                </Card.Description>
+                <br />
+              </Card.Description>
+              <button id="exitIntentExitButton" onClick={close}>
+                <Icon id="exitIntentExitIcon" name="cancel" size="huge" />
+              </button>
+            </Card.Content>
+          </Card>
+        </Modal.Content>
+      </Modal>
     </div>
   ) : null;
 };
