5a7a6b397107ba662789dea3400f68d0dbd1231a
Alex_Bellamy <alexlbellamy@gmail.com>
2021-03-31 19:46:39 +0200

    adding clickable email addresses to desktop about us page as well as clickable linkedin icons

--- src/components/generalPageContent/AboutUs.jsx
+++ src/components/generalPageContent/AboutUs.jsx
@@ -426,7 +426,15 @@ const AboutUs = () => {
                                 size="big"
                                 className="emailButton"
                               />
+                               <p>alex@wattswhat.co.uk</p>
                             </a>
+                            <i
+                              onClick={() =>
+                                window.open("https://www.linkedin.com/in/alex-bellamy-0237441b4/")
+                              }
+                              className="fa fa-linkedin"
+                              aria-hidden="true"
+                            />
                           </Grid.Column>
                           <Grid.Column>
                             <Popup
@@ -455,7 +463,15 @@ const AboutUs = () => {
                                 size="big"
                                 className="emailButton"
                               />
+                              <p>felix@wattswhat.co.uk</p>
                             </a>
+                            <i
+                              onClick={() =>
+                                window.open("https://www.linkedin.com/in/felix-oppong-willborg-827b93b5/")
+                              }
+                              className="fa fa-linkedin"
+                              aria-hidden="true"
+                            />
                           </Grid.Column>
                           <Grid.Column>
                             <Popup
@@ -482,6 +498,7 @@ const AboutUs = () => {
                                 size="big"
                                 className="emailButton"
                               />
+                                <p>hello@wattswhat.co.uk</p>
                             </a>
                           </Grid.Column>
                         </Grid.Row>
